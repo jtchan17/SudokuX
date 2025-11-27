d6e206250b71c86fcbd66cd55bc3c9d9832d4658
jtchan17 <jiatongchan0217@gmail.com>
2025-11-27 12:40:04 +0800

    update program.cs with flow of console application

--- back-end/SudokuDataManager/Program.cs
+++ back-end/SudokuDataManager/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SudokuDataManager{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("=== Sudoku Data Management System ===");
+                Console.WriteLine("1. Player Management");
+                Console.WriteLine("2. Puzzle Management");
+                Console.WriteLine("3. Data Analysis");
+                Console.WriteLine("4. Exit");
+                Console.Write("Choose: ");
+
+            }
+        }
+
+        static void PlayerMenu()
+        {
+            Console.WriteLine("\n=== Player Menu ===");
+            Console.WriteLine("1. Add Player");
+            Console.WriteLine("2. View Players");
+            Console.WriteLine("3. Update Player");
+            Console.WriteLine("4. Delete Player");
+            Console.WriteLine("5. Back");
+            Console.Write("Choose: ");
+            Console.WriteLine();
+        }
+
+        static void PuzzleMenu()
+        {
+            Console.WriteLine("\n=== Puzzle Menu ===");
+            Console.WriteLine("1. Add Puzzle");
+            Console.WriteLine("2. Generate Random Puzzle");
+            Console.WriteLine("3. View Puzzles");
+            Console.WriteLine("4. Delete Puzzle");
+            Console.WriteLine("5. Back");
+            Console.Write("Choose: ");
+        }
+    }
+}
